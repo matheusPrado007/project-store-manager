de6b477055a659d5cc6aada13e2667607aec97dd
Matheus Prado <peu_prado01@hotmail.com>
2023-02-14 00:49:53 -0300

    Requisito - 02 - Desenvolva testes que cubram no mínimo 5% das camadas da sua aplicação

---
+++ tests/unit/controllers/mock/productControllerMock.js
@@ -0,0 +1,21 @@
+const productDataBase = [
+  {
+    id: 1,
+    name: "Martelo de Thor",
+  },
+  {
+    id: 2,
+    name: "Traje de encolhimento",
+  },
+  /* ... */
+];
+
+const productDataId ={
+  id: 1,
+  name: "Martelo de Thor"
+}
+
+module.exports = {
+  productDataBase,
+  productDataId,
+};
